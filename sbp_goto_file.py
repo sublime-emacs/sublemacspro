a2602080e15a166ec66a4f4b168d3185a7a4c1af
Martin Grund <grundprinzip@gmail.com>
2012-10-10 21:15:26 +0200

    Fixing bug in goto_file bootstrapping

--- sbp_goto_file.py
+++ sbp_goto_file.py
@@ -1,23 +1,29 @@
+import os
+import sublime
+
 #bootstrapping
 plugin_path = os.path.dirname(__file__)
 if not os.path.exists(os.path.join(plugin_path, 'GotoOpenFile.py')):
 
-	# Download the file
-	url = "https://raw.github.com/grundprinzip/sublime-goto-open-file/d86ed0230d56d5f6309c7ab6af1ca81c760edc5c/GotoOpenFile.py"
-	crc = "4b54d7c5ff90066e8bc3b414be2bcf55"
-	
-	import urllib
-	from hashlib import md5
+    # Download the file
+    url = "https://raw.github.com/grundprinzip/sublime-goto-open-file/d86ed0230d56d5f6309c7ab6af1ca81c760edc5c/GotoOpenFile.py"
+    crc = "4b54d7c5ff90066e8bc3b414be2bcf55"
+    
+    import urllib
+    from hashlib import md5
 
-	payload = urllib.urlopen(url).read()
+    payload = urllib.urlopen(url).read()
     if md5(payload).hexdigest() != crc:
+        sublime.error_message("Could not install GotoOpenFile.py")
         raise ImportError('Invalid checksum.')
 
 
-     # Open file to write result
-     fid = open(os.path.join(plugin_path, "GotoOpenFile.py"), "w+")
-     fid.write(payload)
-     fid.close
+    # Open file to write result
+    fid = open(os.path.join(plugin_path, "GotoOpenFile.py"), "w+")
+    fid.write(payload)
+    fid.close
+
+    sublime.status_message("Installed GotoOpenFile.py (c)phildopus ")
 
 # end bootstrap
 
