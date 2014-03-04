9776e98e6ed809e7521e9f2fbb0f3baf91ecff31
grundprinzip <grundprinzip@gmail.com>
2014-03-04 15:40:05 +0100

    ST2 compat

--- jove.py
+++ jove.py
@@ -770,6 +770,10 @@ class JoveShowScopeCommand(JoveTextCommand):
         print(status)
         self.view.set_status(JOVE_STATUS, status)
 
+#
+# Advance to the beginning (or end if going backward) word unless already positioned at a word
+# character. This can be used as setup for commands like upper/lower/capitalize words. This ignores
+# the argument count.
 class JoveMoveWordCommand(JoveTextCommand):
     should_reset_target_column = True
     is_ensure_visible_cmd = True
@@ -799,7 +803,7 @@ class JoveMoveWordCommand(JoveTextCommand):
     def call_find_by_class(self, view, point, forward, classes, separators):
       '''
       This is a small wrapper that maps to the right find_by_class call
-      depending on the version of ST2 installed
+      depending on the version of ST installed
       '''
       if _ST3:
         return self.find_by_class_native(view, point, forward, classes, separators)
@@ -833,33 +837,6 @@ class JoveMoveWordCommand(JoveTextCommand):
         for c in range(count):
             jove.for_each_cursor(move_word0, first=(c == 0))
 
-#
-# Advance to the beginning (or end if going backward) word unless already positioned at a word
-# character. This can be used as setup for commands like upper/lower/capitalize words. This ignores
-# the argument count.
-#
-class JoveToWordCommand(JoveTextCommand):
-    should_reset_target_column = True
-
-    def run_cmd(self, jove, direction=1):
-        view = self.view
-
-        settings = view.settings()
-        separators = settings.get("jove_word_separators", default_jove_word_separators)
-        forward = direction > 0
-
-        def to_word(cursor):
-            point = cursor.b
-            if forward:
-                if not jove.is_word_char(point, True, separators):
-                    point = view.find_by_class(point, True, sublime.CLASS_WORD_START, separators)
-            else:
-                if not jove.is_word_char(point, False, separators):
-                    point = view.find_by_class(point, False, sublime.CLASS_WORD_END, separators)
-            cursor.a = cursor.b = point
-            return cursor
-
-        jove.for_each_cursor(to_word)
 
 class JoveCaseRegion(JoveTextCommand):
 
@@ -1795,7 +1772,7 @@ class ISearchInfo():
         self.set_text(self.current.search)
 
     def cancel(self):
-        self.view.window().run_command("hide_panel")
+        sublime.active_window().run_command("hide_panel")
         self.finish(abort=True)
 
     def quit(self):
